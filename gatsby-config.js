1c39c9877d3a5990e6c5edffc07901f6d96fc8f0
dkellam52 <Dominique.kellam@icloud.com>
2024-02-13 09:54:41 -0500

    Finished Gatsby Tutorial Part 2

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `cd Desktop`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
